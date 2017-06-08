fa59fadedc9bf141fb18a9583fff5eb5b10d3949
Kamil Mangold <kamilmangold@gmail.com>
2017-06-08 18:40:50 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var a = 2;
+
+var b = 10;
+
+var value = (a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+	if (value > 0) {
+
+    console.log('Wynik dodatni')//Code to execute if condition1 is true
+
+} else if (value < 0) {
+
+    console.log('Wynik ujemny')//Code to execute if condition2 is true
+
+} else {
+
+    console.log('Wynik równy 0')//Code to execute if none of the previous conditions prove to be true
+
+}
